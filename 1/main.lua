d2f21969cdbe165800d7e06bf33cbd5e255841ef
unonu <unonuorea@gmail.com>
2015-03-23 19:42:48 -0400

    tightened up the graphics on lvl 3, added screenshake

--- 1/main.lua
+++ 1/main.lua
@@ -1,3 +1,9 @@
+--[[ I detected that your phone has a monopedosautarometer. Please jump up and down on one foot to see
+if it's working properly. ]]
+
+--[[ With patience and a little bit of speed, even these lumps of iron can break free of gravity's grip
+and reach the heavens. - Plato ]]
+
 -- Checks if two line segments intersect. Line segments are given in form of ({x,y},{x,y}, {x,y},{x,y}).
 function math.checkIntersect(l1p1, l1p2, l2p1, l2p2)
     local function checkDir(pt1, pt2, pt3) return math.sign(((pt2[1]-pt1[1])*(pt3[2]-pt1[2])) - ((pt3[1]-pt1[1])*(pt2[2]-pt1[2]))) end
@@ -59,7 +65,7 @@ function ball.make(x,y,iV)
 	b.speed = 10
 	b.forces = {iV}
 	b.vec = {0,0} --<-- I think it looks like an owl
-	b.gravity = .565
+	b.gravity = .986
 
 	b.color = {math.random(128,255),math.random(128,255),math.random(128,255)}
 
@@ -73,14 +79,14 @@ end
 function ball:draw()
 	for i=1, #self.trail do
 		love.graphics.setColor(255,255,255,255* i/#self.trail)
-		love.graphics.setLineWidth(8* i/#self.trail)
+		love.graphics.setLineWidth(16* i/#self.trail)
 		local x,y = unpack(self.trail[i+1] or {self.x, self.y})
 		love.graphics.line(x,y,unpack(self.trail[i]))
 	end
 	love.graphics.setLineWidth(1)
 
 	love.graphics.setColor(unpack(self.color))
-	love.graphics.circle("fill",self.x, self.y, 6, 12)
+	love.graphics.circle("fill",self.x, self.y, 12, 12)
 end
 
 function ball:update(dt)
@@ -101,11 +107,11 @@ function ball:update(dt)
 	self.y = self.y + self.vec[2]*dt
 
 	-- if self.vec[1] ~= 0 then self.vec[1] = self.vec[1] - self.vec[1] / (99 * dt) end
-	-- if self.vec[1] ~= 0 then self.vec[1] = self.vec[1] - self.vec[1]* self.speed/6000 end
+	if self.vec[1] ~= 0 then self.vec[1] = self.vec[1] - self.vec[1]* self.speed/6000 end
 	-- print(self.speed/6000)
 	if math.abs(self.vec[1]) < .001 then self.vec[1] = 0 end
 	if math.abs(self.vec[2]) < .001 then self.vec[2] = 0 end
-	-- self.vec[2] = self.vec[2] + (self.gravity/(.01*dt)) * self.speed
+	self.vec[2] = self.vec[2] + self.gravity
 	-- self.vec[2] = self.vec[2] + self.gravity / self.speed
 
 	--collision
@@ -113,7 +119,7 @@ function ball:update(dt)
 		local b = bodies[i]
 		if math.checkIntersect({self.x,self.y},{self._x,self._y},b[1],b[2]) then
 			-- local z = math.max((self.vec[1]^2 + self.vec[2]^2)^(0.5) * (b[3] or 1), 18)
-			local z = math.max((self.vec[1]^2 + self.vec[2]^2)^(0.5) + .2, 18)
+			local z = math.max((self.vec[1]^2 + self.vec[2]^2)^(0.5) + 1, 18) * (b[3] or 1)
 			local deflection = {math.deflect({b[1][1],b[1][2],b[2][1],b[2][2]},{self.x,self.y,self._x,self._y})}
 			self.vec[1], self.vec[2] = z*deflection[1], z*deflection[2]
 			self.x, self.y = math.getIntercept({self.x,self.y},{self._x,self._y},b[1],b[2])
@@ -125,24 +131,25 @@ function ball:update(dt)
 	for i=l, 1, -1 do
 		local b = edges[i]
 		if b and math.checkIntersect({self.x,self.y},{self._x,self._y},b[1],b[2]) then
-			local z = math.max((self.vec[1]^2 + self.vec[2]^2)^(0.5) + .2, 18)
+			local z = math.max((self.vec[1]^2 + self.vec[2]^2)^(0.5) + 1, 18)
 			local deflection = {math.deflect({b[1][1],b[1][2],b[2][1],b[2][2]},{self.x,self.y,self._x,self._y})}
 			self.vec[1], self.vec[2] = z*deflection[1], z*deflection[2]
 			self.x, self.y = math.getIntercept({self.x,self.y},{self._x,self._y},b[1],b[2])
 			self.x, self.y = self.x + deflection[1], self.y + deflection[2]
 			bricks:collided(math.floor((i-1)/4)+1)
+			shake(.2)
 			break
 		end
 	end
 	for i=1, #balls do
 		local b = balls[i]
-		if b and b~= self and math.dist(self.x,self.y,b.x,b.y) <= 6 then
+		if b and b~= self and math.dist(self.x,self.y,b.x,b.y) <= 12 then
 			self.vec,b.vec = b.vec,self.vec
 		end
 	end
-	if self.y < 0 then self.y = 1; self:push(math.pi/2,-self.vec[2]*2) end
-	if self.x < 0 then self.x = 0; self:push(0,-self.vec[1]*2) end
-	if self.x > width then self.x = width; self:push(math.pi,self.vec[1]*2) end
+	if self.y < 8 then self.y = 9; self:push(math.pi/2,-self.vec[2]*2) end
+	if self.x < 8 then self.x = 8; self:push(0,-self.vec[1]*2) end
+	if self.x > width-8 then self.x = width-8; self:push(math.pi,self.vec[1]*2) end
 	if self.y > height then self.kill = true end
 end
 
@@ -157,18 +164,22 @@ function brickyard.make(x,y,w,h,list)
 
 	b.width = w
 	b.height = h
+	b.bWidth = 64
+	b.bHeight = 32
 	b.rows = {}
 	b.edges = {}
 	for i=1,h do
 		local r = {}
 		for j=1,w do
 			r[j] = tonumber(list:sub(((i-1)*w) + j,((i-1)*w) + j))
-			local _x = x + (j-1)*24
-			local _y = y + (i-1)*12
-			b.edges[(i-1)*w*4 + (j-1)*4 + 1] = {{_x,_y},{_x+24,_y}}
-			b.edges[(i-1)*w*4 + (j-1)*4 + 2] = {{_x+24,_y},{_x+24,_y+12}}
-			b.edges[(i-1)*w*4 + (j-1)*4 + 3] = {{_x,_y+12},{_x+24,_y+12}}
-			b.edges[(i-1)*w*4 + (j-1)*4 + 4] = {{_x,_y},{_x,_y+12}}
+			local _x = x + (j-1)*b.bWidth
+			local _y = y + (i-1)*b.bHeight
+			if r[j] ~= 0 then
+				b.edges[(i-1)*w*4 + (j-1)*4 + 1] = {{_x,_y},{_x+b.bWidth,_y}}
+				b.edges[(i-1)*w*4 + (j-1)*4 + 2] = {{_x+b.bWidth,_y},{_x+b.bWidth,_y+b.bHeight}}
+				b.edges[(i-1)*w*4 + (j-1)*4 + 3] = {{_x,_y+b.bHeight},{_x+b.bWidth,_y+b.bHeight}}
+				b.edges[(i-1)*w*4 + (j-1)*4 + 4] = {{_x,_y},{_x,_y+b.bHeight}}
+			end
 		end
 		b.rows[i] = r
 	end
@@ -183,17 +194,18 @@ function brickyard:draw()
 	for j=1, self.height do
 	for i=1, self.width do
 		if self.rows[j][i] ~= 0 then
-			love.graphics.rectangle("fill", (i-1)*24, (j-1)*12, 22, 10)
+			-- love.graphics.rectangle("fill", (i-1)*self.bWidth, (j-1)*self.bHeight, self.bWidth - 2, self.bHeight - 2)
+			love.graphics.draw(brickImage, (i-1)*self.bWidth, (j-1)*self.bHeight)
 		end
 	end
 	end
 	love.graphics.pop()
-	love.graphics.setColor(255,0,0)
-	for i=1, self.width*self.height*4 do
-		if self.edges[i] then
-			love.graphics.line(self.edges[i][1][1],self.edges[i][1][2],self.edges[i][2][1],self.edges[i][2][2])
-		end
-	end
+	-- love.graphics.setColor(255,0,0)
+	-- for i=1, self.width*self.height*4 do
+	-- 	if self.edges[i] then
+	-- 		love.graphics.line(self.edges[i][1][1],self.edges[i][1][2],self.edges[i][2][1],self.edges[i][2][2])
+	-- 	end
+	-- end
 end
 
 function brickyard:update(dt)
@@ -214,21 +226,38 @@ function brickyard:getEdges()
 	return self.edges, self.width*self.height*4
 end
 
+function shake(t) screenshake = t end
+
 function love.load()
 	love.graphics.setBackgroundColor(0,0,0)
 	balls = {}
-	love.window.setMode(256,512)
-	width = 256
-	height = 512
-	flippers = {false, false,-math.pi/16,-math.pi/16,48,48,{{64,480},{112,480},.5},{{192,480},{144,480},.5},}
-	bodies = {{{0,440},{64,480},.5},{{256,440},{192,480},.5},flippers[7],flippers[8]}
-	bricks = brickyard.make(24,24,8,6,"111111111111111111111111111111111111111111111111")
+	love.window.setMode(640,960,{borderless = true})
+	width = 640
+	height = 960
+	flippers = {false, false,-math.pi/6,-math.pi/6,148,148,{{163,848},{311,848},.25},{{width-163,848},{width-311,848},.25},}
+	bodies = {	{{-10,747},{163,848},.5},
+				{{width+10,747},{width-163,848},.5},
+				flippers[7],
+				flippers[8]}
+	bricks = brickyard.make(24,24,9,12,"111111111000000000011000110111101111111111111111111111011111110001111100000111000000010000000000000111111111")
+
+	bg = love.graphics.newImage("board.png")
+	brickImage = love.graphics.newImage("brick.png")
+
+	screenshake = 0
 
 	launchAngle = -math.pi/2
 end
 
 function love.draw()
+
+	if screenshake > 0 then
+		love.graphics.push()
+		love.graphics.translate(math.random(-4,4),math.random(-4,4))
+	end
+
 	love.graphics.setColor(255, 255, 255)
+	love.graphics.draw(bg)
 	for i=1, #bodies do
 		love.graphics.line(bodies[i][1][1], bodies[i][1][2], bodies[i][2][1], bodies[i][2][2])
 	end
@@ -240,35 +269,37 @@ function love.draw()
 	love.graphics.line(flippers[7][1][1],flippers[7][1][2],flippers[7][2][1],flippers[7][2][2])
 	love.graphics.line(flippers[8][1][1],flippers[8][1][2],flippers[8][2][1],flippers[8][2][2])
 
-	love.graphics.line(128,512,128 + 64*math.cos(launchAngle), 512 + 64*math.sin(launchAngle))
+	love.graphics.line(320,960,320 + 64*math.cos(launchAngle), 960 + 64*math.sin(launchAngle))
 
-	love.graphics.print(#balls)
+	if screenshake > 0 then
+		love.graphics.pop()
+	end
+	love.graphics.print(#balls..' '..love.timer.getFPS())
 end
 
 function love.update(dt)
 
-	launchAngle = math.atan2(love.mouse.getY()-512, love.mouse.getX()-128)
-
-
+	launchAngle = math.atan2(love.mouse.getY()-960, love.mouse.getX()-320)
+	screenshake = math.max(screenshake - dt, 0)
 
 	if love.keyboard.isDown("lshift") then
 		flippers[1] = true
 		flippers[3] = math.pi/3
 	else
 		flippers[1] = false
-		flippers[3] = -math.pi/16
+		flippers[3] = -math.pi/6
 	end
-		flippers[7][1] = {64,480}
-		flippers[7][2] = {64 + flippers[5]*math.cos(flippers[3]), 480 - flippers[5]*math.sin(flippers[3])}
+		flippers[7][1] = {163,848}
+		flippers[7][2] = {163 + flippers[5]*math.cos(flippers[3]), 848 - flippers[5]*math.sin(flippers[3])}
 	if love.keyboard.isDown("rshift") then
 		flippers[2] = true
 		flippers[4] = math.pi/3
 	else
 		flippers[2] = false
-		flippers[4] = -math.pi/16
+		flippers[4] = -math.pi/6
 	end
-		flippers[8][1] = {192,480}
-		flippers[8][2] = {192 - flippers[6]*math.cos(flippers[4]), 480 - flippers[6]*math.sin(flippers[4])}
+		flippers[8][1] = {width-163,848}
+		flippers[8][2] = {width-163 - flippers[6]*math.cos(flippers[4]), 848 - flippers[6]*math.sin(flippers[4])}
 
 
 	local kills = 0
@@ -295,7 +326,7 @@ end
 
 function love.keypressed(k)
 	if k == ' ' then
-		balls[#balls + 1] = ball.make(128,512,{launchAngle,18})
+		balls[#balls + 1] = ball.make(320,960,{launchAngle,100})
 	elseif k == 'escape' then
 		love.event.quit()
 	end
